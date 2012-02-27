baa775d12d27839f98f9e91ee6cefef7c54a7824
Phil Baines <phil.baines@wubbleyew.com>
2012-02-27 09:58:34 +0000

    Fix order of nancy gets

--- src/Wubblog.Web/Modules/PageModule.cs
+++ src/Wubblog.Web/Modules/PageModule.cs
@@ -26,18 +26,18 @@ namespace Wubblog.Web.Modules
 				return View["About"];
 			};
 			
-			Get["/{reference}/"] = parameters =>
-			{
-				var entry = Entry.GetByReference(parameters.reference);
-				return View["Entry", entry];
-			};
-			
 			Get["/entry/{id}"] = parameters =>
 			{
 				var entry = Entry.GetById(parameters.id);
 				var entryFormat = "{{ id: {0}, title: {1}, description: {2}, markdown: {3}, html: {4}, keywords: {5}}}";
 				var entryData = string.Format(entryFormat, entry.EntryId, entry.Title, entry.Description, entry.Markdown, entry.Html, entry.Keywords);
 				return entryData;
+			};
+			
+			Get["/{reference}/"] = parameters =>
+			{
+				var entry = Entry.GetByReference(parameters.reference);
+				return View["Entry", entry];
 			};
 		}
 	}
