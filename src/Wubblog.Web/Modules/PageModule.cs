2627ded59e54c265e9e3bce46f510cdca64121b8
Phil Baines <phil.baines@wubbleyew.com>
2012-02-27 10:42:16 +0000

    using Response.AsJson

--- src/Wubblog.Web/Modules/PageModule.cs
+++ src/Wubblog.Web/Modules/PageModule.cs
@@ -28,10 +28,8 @@ namespace Wubblog.Web.Modules
 			
 			Get["/entry/{id}"] = parameters =>
 			{
-				var entry = Entry.GetById(parameters.id);
-				var entryFormat = "{{ id: {0}, title: {1}, description: {2}, markdown: {3}, html: {4}, keywords: {5}}}";
-				var entryData = string.Format(entryFormat, entry.EntryId, entry.Title, entry.Description, entry.Markdown, entry.Html, entry.Keywords);
-				return entryData;
+				Entry entry = Entry.GetById(parameters.id);
+				return Response.AsJson(entry);
 			};
 			
 			Get["/{reference}/"] = parameters =>
