8782bf770b18b9160b74276625a739db1b59213b
Phil Baines <phil.baines@wubbleyew.com>
2012-03-13 14:50:06 +0000

    Add Wubblog.Tests - first test new Author password hashing. Change version numbers.

---
+++ src/Wubblog.Tests/AuthorFixtures.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using NUnit.Framework;
+using Wubblog.Library;
+
+namespace Wubblog.Tests
+{
+	[TestFixture]
+	public class AuthorFixtures
+	{
+		[Test]
+		public void ValidateCorrectPassword()
+		{
+			var author = new Author("joe", "test1234");
+			var passwordHash = author.PasswordHash;
+			
+			Assert.IsTrue(author.ValidatePassword("test1234"), "Author.ValidatePassword() does not pass a correct password.");
+		}
+		
+		[Test]
+		public void ValidateIncorrectPassword()
+		{
+			var author = new Author("joe", "test1234");
+			var passwordHash = author.PasswordHash;
+			
+			Assert.IsFalse(author.ValidatePassword("test4567"), "Author.ValidatePassword() does not fail an incorrect password");
+		}
+	}
+}
--- src/Wubblog.Tests/Properties/AssemblyInfo.cs
+++ src/Wubblog.Tests/Properties/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Wubblog.Admin")]
+[assembly: AssemblyTitle("Wubblog.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Wubblog.Admin")]
+[assembly: AssemblyProduct("Wubblog.Tests")]
 [assembly: AssemblyCopyright("Copyright 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
