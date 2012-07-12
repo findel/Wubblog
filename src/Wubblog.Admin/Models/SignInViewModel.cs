fea41699da73046910b4930cb6d1fb128fcb0b70
Phil Baines <phil.baines@wubbleyew.com>
2012-07-12 21:14:41 +0100

    Shall we have a sign in page here? Me thinks yes.

---
+++ src/Wubblog.Admin/Models/SignInViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Wubblog.Admin.Models
+{
+	/// <summary>
+	/// Description of SignInViewModel.
+	/// </summary>
+	public class SignInViewModel
+	{
+		public SignInViewModel(){}
+		
+		public string EmailAddress { get; set; }
+		
+		public string Password { get; set; }
+	}
+}
