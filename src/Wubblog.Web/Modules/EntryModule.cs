7f4a6e8b7711659b15fd58e240487e48545fea8e
Phil Baines <phil.baines@wubbleyew.com>
2012-07-29 19:25:55 +0100

    Moving entries into /entries url base

---
+++ src/Wubblog.Web/Modules/EntryModule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Nancy;
+using Wubblog.Library;
+
+namespace Wubblog.Web.Modules
+{
+	public class EntryModule : NancyModule
+	{
+		public EntryModule() : base("/entries")
+		{
+			
+			Get["/{reference}/"] = parameters =>
+			{
+				var entry = Entry.FindByReference(parameters.reference);
+				return View["Entry", entry];
+			};
+			
+			Post["/{reference}/"] = parameters =>
+			{
+				var entry = Entry.FindByReference(parameters.reference);
+				var comment = new Comment(entry);
+				
+				comment.Name = Request.Form.Name;
+				comment.Markdown = Request.Form.Markdown;
+				comment.Email = Request.Form.Email;
+				comment.Twitter = Request.Form.Twitter;
+				comment.Website = Request.Form.Website;
+				comment.Save();
+				
+				return View["Entry", entry];
+				
+			};
+			
+		}
+	}
+}
--- src/Wubblog.Web/Modules/PageModule.cs
+++ src/Wubblog.Web/Modules/PageModule.cs
@@ -6,9 +6,6 @@ using Wubblog.Web.Models;
 
 namespace Wubblog.Web.Modules
 {
-	/// <summary>
-	/// Description of PagesModule.
-	/// </summary>
 	public class PageModule : NancyModule
 	{
 		public PageModule()
@@ -28,28 +25,6 @@ namespace Wubblog.Web.Modules
 			Get["/about/"] = parameters =>
 			{
 				return View["About"];
-			};
-			
-			Get["/{reference}/"] = parameters =>
-			{
-				var entry = Entry.FindByReference(parameters.reference);
-				return View["Entry", entry];
-			};
-			
-			Post["/{reference}/"] = parameters =>
-			{
-				var entry = Entry.FindByReference(parameters.reference);
-				var comment = new Comment(entry);
-				
-				comment.Name = Request.Form.Name;
-				comment.Markdown = Request.Form.Markdown;
-				comment.Email = Request.Form.Email;
-				comment.Twitter = Request.Form.Twitter;
-				comment.Website = Request.Form.Website;
-				comment.Save();
-				
-				return View["Entry", entry];
-				
 			};
 		}
 	}
